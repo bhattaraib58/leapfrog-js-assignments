6631850690c5b24535c80ef8b63e45731edd0163
Biplap Bhattarai <bhattaraib58@gmail.com>
2019-06-21 12:24:20 +0545

    button center align fixed

--- Assignment 1/script.js
+++ Assignment 1/script.js
@@ -10,9 +10,11 @@ var DEFAULT_TRANSITION = 5;
 var DEFAULT_INTERVAL = 1000;
 function Carousel(carouselId, transitionSpeed, interval) {
 
-    // get caraousel and set the values;
-    // at 0 index children for carasoul
+    // get caraousel wrapper and set the value relative for positioning
     var parentCarousel = document.getElementById(carouselId);
+    parentCarousel.style.position = 'relative';
+
+    //get main carasouel
     var carousel = parentCarousel.children[0];
     carousel.style.position = 'absolute';
     carousel.style.zIndex = 10;
@@ -46,17 +48,9 @@ function Carousel(carouselId, transitionSpeed, interval) {
     var animationFrame = 0;
     var carasouelLeftSpace = 0;
 
-
-    var controlElements = createControlElements();
-    controlElements.style.width = '100%';
-    controlElements.style.height = 0;
-    controlElements.style.position = 'absolute';
-    controlElements.style.top = 0;
-    controlElements.style.left = 0;
-    controlElements.style.zIndex = 20;
-
-    parentCarousel.style.position = 'relative';
-    parentCarousel.appendChild(controlElements);
+    //add control elements e.g:- left, right, circles- indexes
+    var controlElement = createControlElements();
+    parentCarousel.appendChild(controlElement);
 
     //call resize for checking the dom changes
     resize();
@@ -72,7 +66,7 @@ function Carousel(carouselId, transitionSpeed, interval) {
         var height = parseInt(window.getComputedStyle(carouselChildren[0]).height) + "px";
         carousel.style.paddingBottom = height;
         parentCarousel.style.paddingBottom = height;
-        controlElements.style.paddingBottom = height;
+        controlElement.style.paddingBottom = height;
     }
 
     //slideshow function
@@ -106,13 +100,20 @@ function Carousel(carouselId, transitionSpeed, interval) {
         button.style.border = 'none';
         button.style.backgroundColor = 'transparent';
         button.innerHTML = buttonData;
-        button.style.position = 'absolute';
         return button;
     }
 
     function createControlElements() {
+        // create main controlElement parent
         var controlElement = document.createElement('div');
+        controlElement.style.width = '100%';
+        controlElement.style.height = 0;
+        controlElement.style.position = 'absolute';
+        controlElement.style.top = 0;
+        controlElement.style.left = 0;
+        controlElement.style.zIndex = 20;
 
+        //left button create and function for transition
         var leftButton = createButton(`
                             <svg viewBox="0 0 16000 16000">
                                 <polyline class="a" points="11040,1920 4960,8000 11040,14080 "></polyline>
@@ -120,34 +121,57 @@ function Carousel(carouselId, transitionSpeed, interval) {
 
         leftButton.style.top = '50%';
         leftButton.style.left = 0;
+        leftButton.style.position = 'absolute';
 
         leftButton.onclick = function () {
-            var currentIndex = parseInt(Math.abs(carasouelLeftSpace) / carasouelWidth);
-            carasouelLeftSpace = -((currentIndex) * carasouelWidth);
+            if (!(carasouelLeftSpace > 0)) {
+                var currentIndex = parseInt(Math.abs(carasouelLeftSpace) / carasouelWidth);
+                carasouelLeftSpace = -((currentIndex) * carasouelWidth);
+            }
         };
 
+        //right button create and function for transition
         var rightButton = createButton(`
                             <svg viewBox="0 0 16000 16000">
                                 <polyline class="a" points="4960,1920 11040,8000 4960,14080 "></polyline>
                             </svg>`);
         rightButton.style.right = 0;
         rightButton.style.top = '50%';
+        rightButton.style.position = 'absolute';
 
         rightButton.onclick = function () {
-            var currentIndex = parseInt(Math.abs(carasouelLeftSpace) / carasouelWidth);
-            carasouelLeftSpace = -((currentIndex + 1) * carasouelWidth);
+            if (!((carasouelLeftSpace - carasouelWidth) < -(carasouelWidth * carouselChildren.length))) {
+                var currentIndex = parseInt(Math.abs(carasouelLeftSpace) / carasouelWidth);
+                carasouelLeftSpace = -((currentIndex + 1) * carasouelWidth);
+            }
         };
 
+        //add left button and right button to control element
         controlElement.appendChild(leftButton);
         controlElement.appendChild(rightButton);
 
+        var indexWidth = 7;
+        var indexHeight = 7;
+        var indexMarginBetween = 2;
+
+        var indexWrapper = document.createElement('div');
+        indexWrapper.style.position = 'absolute';
+        indexWrapper.style.bottom = 0;
+        indexWrapper.style.left = 50 + '%';
+        indexWrapper.style.marginLeft = -(carouselChildren.length * (indexWidth + indexMarginBetween)) / 2 + 'px';
+
+        //create array of below navigation buttons
         for (var i = 0; i < carouselChildren.length; i++) {
             var indexButton = createButton(i + 1);
             indexButton.style.border = '1px solid black';
-            indexButton.style.borderRadius=50+'%';
-            indexButton.style.backgroundColor='gray';
-            indexButton.style.color='white';
-            indexButton.style.marginRight = 10 + 'px';
+            indexButton.style.borderRadius = 50 + '%';
+            indexButton.style.backgroundColor = 'gray';
+            indexButton.style.color = 'white';
+            indexButton.style.marginRight = indexMarginBetween + 'px';
+            indexButton.style.width = indexWidth + 'px';
+            indexButton.style.height = indexHeight + 'px';
+            indexButton.style.fontSize = (indexHeight-4) + 'px';
+            indexButton.style.textAlign = 'center';
             indexButton.style.bottom = 0;
             indexButton.onclick = function (i) {
                 return function () {
@@ -156,12 +180,16 @@ function Carousel(carouselId, transitionSpeed, interval) {
             }(i);
 
             indexButton.style.left = 50 + ((i + 1) * 10) + '%';
-            controlElement.appendChild(indexButton);
+            indexWrapper.appendChild(indexButton);
         }
+        controlElement.appendChild(indexWrapper);
+
+
         return controlElement;
     }
 }
 
+//carasouel object based on the transition interval and carasoul object
 var a = new Carousel('test1', 5, 200);
 var b = new Carousel('test2', 1, 100);
 var c = new Carousel('test3', 1, 1000);
